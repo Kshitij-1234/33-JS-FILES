fb0095ce38972065888184ae2464689172af1c66
Kshitij-1234 <263807826+Kshitij-1234@users.noreply.github.com>
2026-06-03 05:51:24 +0000

    assignment 3

---
+++ assignment-3-equalsigns.js
@@ -0,0 +1,15 @@
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(null == undefined);
+console.log(null === undefined);
+
+
+//== vs === 
+
+/*
+== it is less strict than ===.in first it checks value and then checkks its type. if value is same then it returns true even if type is different.
+
+=== it is more strict than ==. in first it checks type and then checks value. if type is different then it returns false even if value is same.
+
+
+*/ 
