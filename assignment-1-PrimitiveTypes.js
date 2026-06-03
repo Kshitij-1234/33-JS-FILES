f9540fc834ce02dd84510c61ae04b8e04b11dace
Kshitij-1234 <263807826+Kshitij-1234@users.noreply.github.com>
2026-06-03 05:51:24 +0000

    assignment 1

---
+++ assignment-1-PrimitiveTypes.js
@@ -0,0 +1,34 @@
+/*Create variables for:
+
+name
+age
+isStudent
+salary
+emptyValue
+unknownValue
+
+Then print their types using:
+
+typeof
+
+Expected learning:
+
+primitive types
+typeof
+null weirdness*/
+
+
+let name="Kshitij";
+age=18;
+isStudent=Symbol("At Sinhagad");
+salary=1n;
+emptyValue = null;
+unknownValue=undefined;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof salary);
+console.log(typeof emptyValue);
+console.log(typeof unknownValue);
+
